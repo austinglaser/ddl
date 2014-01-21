55efe2c59c173cee0975afae46eaf203b50b5754
Austin Glaser <austin.glaser@gmail.com>
2014-01-21 13:59:49 -0700

    final prelab version

--- labs/lab2/prelab/austin_prelab.c
+++ labs/lab2/prelab/austin_prelab.c
@@ -1,9 +1,22 @@
+// ECEN 3000 (Lab 2 Prelab)
+// Austin Glaser and Jesse Ellison
+// This is an implementation of a decimal-to-morse
+// conversion function, with an integrated test.
+
 #include <string.h>
 #include <stdint.h>
+#include <assert.h>
 #include <stdio.h>
 
-void decimal_to_morse(uint8_t dec, uint8_t morse_out[])
+// Takes an unsigned integer input and converts it into
+// the morse code equivalent (returning an array where
+// 1 is a dash (long) and 0 is a dot (short).
+//
+// Uses a lookup table to speed up the conversion
+int decimal_to_morse(uint8_t dec, uint8_t morse_out[])
 {
+  if (dec > 9) return -1;
+
   uint8_t morse_lookup[10][5] = {
                                  {1, 1, 1, 1, 1},  //0
                                  {0, 1, 1, 1, 1},  //1
@@ -18,6 +31,8 @@ void decimal_to_morse(uint8_t dec, uint8_t morse_out[])
                                 };
 
   memcpy(morse_out, morse_lookup[dec], 5);
+
+  return 0;
 }
 
 int main(void)
@@ -26,12 +41,14 @@ int main(void)
   int32_t i, j;
 
   for (i = 0; i <= 9; i++) {
-    decimal_to_morse(i, morse_out);
+    assert(!decimal_to_morse(i, morse_out));
 
     printf("%d -> ", i);
     for (j = 0; j < 5; j++) printf("%d ", morse_out[j]);
     printf("\n");
   }
+
+  assert(decimal_to_morse(50, morse_out));
 
   return 0;
 }
