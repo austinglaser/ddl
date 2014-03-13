1ced6f09ba79545b28486ca72376b5735b5dc564
Austin Glaser <austin.glaser@gmail.com>
2014-03-12 21:35:55 -0600

    working fir, doesn't fit at order 465

--- labs/lab6/fir_fpga/multiply.v
+++ labs/lab6/fir_fpga/multiply.v
@@ -4,14 +4,18 @@ module multiply (
   mult_2,
   product
 );
+  // parameters; widths of operands
 	parameter width_1 = 10;
 	parameter width_2 = 17;
 	
+  // define arguments. Note: output width must be at minimum
+  // the sum of the two input widths
 	input clock;
 	input  [width_1-1:0] mult_1;
 	input  [width_2-1:0] mult_2;
 	output reg [(width_1 + width_2)-1:0] product;
 	
+  // multiply on clock edge
 	always @(posedge clock) product <= (mult_1 * mult_2);
 	
 endmodule
--- labs/lab6/fir_fpga/shift_reg.v
+++ labs/lab6/fir_fpga/shift_reg.v
@@ -3,13 +3,18 @@ module shift_reg (
   data_in,
   data_out
 );
+  // parameters; width and length of reg
   parameter width = 10;
   parameter length = 5;
   
+  // define input arguments. We allow all
+  // values in the register to be accessible
 	input clock;
 	input [width-1:0]  data_in;
 	output reg [width-1:0] data_out[length - 1:0];
 
+  // first shifter must be defined manually; 
+  // subsequent ones must be generated
 	shift U0(clock, data_in, data_out[0]);
 	
 	genvar i;
